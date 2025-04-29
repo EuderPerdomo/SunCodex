0f9643a33cfa9ea84cc32b5c7128a5fc88908611
PcTrabajo <euderfabian1996@gmail.com>
2025-04-29 13:06:24 -0500

    Cambios agrego panel admin 29/04/2025:13:06

---
+++ admin/src/app/components/blog/index-blog/index-blog.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-index-blog',
+  standalone: true,
+  imports: [],
+  templateUrl: './index-blog.component.html',
+  styleUrl: './index-blog.component.css'
+})
+export class IndexBlogComponent {
+
+}
